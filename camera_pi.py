9ec9f4b0e9eecb2ac261a956ea0951760c08257b
Mrmachine3 <atone36@gmail.com>
2018-12-24 01:33:54 -0600

    updated syntax error in camera_pi.py

--- camera_pi.py
+++ camera_pi.py
@@ -42,23 +42,26 @@ class Camera(object):
         with picamera.PiCamera() as camera:
             # camera setup
             camera.resolution = (320, 240)
-            camera.hflip = True 
-            camera.vflip = True 
+            camera.hflip = True
+            camera.vflip = True
 
             # let camera warm up
             camera.start_preview()
             time.sleep(2)
 
-            stream - io.BytesIO()
+            stream = io.BytesIO()
             for foo in camera.capture_continuous(stream, 'jpeg',
                                                  use_video_port=True):
+                # store frame
+                stream.seek(0)
+                cls.frame = stream.read()
 
-            # store frame
-            stream.seek(0)
-            stream.truncate()
+                # reset stream for next frame
+                stream.seek(0)
+                stream.truncate()
 
-            # if there hasn't been any clients asking for frame in
-            # the last 10 seconds stop the thread
-            if time.time() - cls.last_access > 10:
-                break
+                # if there hasn't been any clients asking for frames in
+                # the last 10 seconds stop the thread
+                if time.time() - cls.last_access > 10:
+                    break
         cls.thread = None
