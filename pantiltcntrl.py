ab25493717c3f22375cec8e35a21c28fde903542
Mrmachine3 <atone36@gmail.com>
2018-12-24 02:20:16 -0600

    added servo control script

---
+++ pantiltcntrl.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+###########################################################################
+# Script Name: pantiltcntrl.py
+# Create Date: 12/24/2018
+# Description: The purpose of this script is to remotely control the pan-tilt
+# servo angle of PiCamera. 
+# Author: Mr. Machine
+# Tags: hardware, IoT, servos,  
+########################################################################### 
+
+# Library imports
+from time import sleep
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+def setServoAngle(servo, angle):
+    pwm = GPIO.PWM(servo, 50)
+    pwm.start(8)
+    dutycycle = angle / 18. + 3.
+    pwm.ChangeDutyCycle(dutyCycle)
+    sleep(0.3)
+    pwm.stop()
+
+if __name__ == '__main__':
+    import sys
+    servo = int(sys.argv[1]
+    GPIO.setup(servo, GPIO.OUT)
+    setServoAngle(servo, int(sys.argv[2]))
+    GPIO.cleanup()
