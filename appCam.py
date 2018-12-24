3a5933ad98e94942e0032bad672c9ebcfba176b1
Mrmachine3 <atone36@gmail.com>
2018-12-24 01:30:47 -0600

    updated syntax error in appcam.py

--- appCam.py
+++ appCam.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Video streaming home page."""
-    reaturn render_template('index.html')
+    return render_template('index.html')
 def gen(camera):
     """Video streaming generator function."""
     while True:
