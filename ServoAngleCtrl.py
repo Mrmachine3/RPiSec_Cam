bc41a80fe6a07fdfddb24a1473b5f4d90fa25fe3
Mrmachine3 <atone36@gmail.com>
2018-12-24 02:28:57 -0600

    updated servo angle control script

--- ServoAngleCtrl.py
+++ ServoAngleCtrl.py
@@ -16,16 +16,17 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 def setServoAngle(servo, angle):
+    assert angle >=30 and angle <= 150
     pwm = GPIO.PWM(servo, 50)
     pwm.start(8)
-    dutycycle = angle / 18. + 3.
+    dutyCycle = angle / 18. + 3.
     pwm.ChangeDutyCycle(dutyCycle)
     sleep(0.3)
     pwm.stop()
 
 if __name__ == '__main__':
     import sys
-    servo = int(sys.argv[1]
+    servo = int(sys.argv[1])
     GPIO.setup(servo, GPIO.OUT)
     setServoAngle(servo, int(sys.argv[2]))
     GPIO.cleanup()
