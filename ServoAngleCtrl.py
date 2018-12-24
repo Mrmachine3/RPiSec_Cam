3e7dda3db613431492d3a82c7a7b90dad3cc1090
Mrmachine3 <atone36@gmail.com>
2018-12-24 02:39:46 -0600

    updated servo script

--- ServoAngleCtrl.py
+++ ServoAngleCtrl.py
@@ -15,6 +15,12 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+pan = 27
+tilt = 17
+
+GPIO.setup(tilt, GPIO.OUT) # white => TILT
+GPIO.setup(pan, GPIO.OUT) # gray ==> PAN
+
 def setServoAngle(servo, angle):
     assert angle >=30 and angle <= 150
     pwm = GPIO.PWM(servo, 50)
@@ -25,8 +31,11 @@ def setServoAngle(servo, angle):
     pwm.stop()
 
 if __name__ == '__main__':
-    import sys
-    servo = int(sys.argv[1])
-    GPIO.setup(servo, GPIO.OUT)
-    setServoAngle(servo, int(sys.argv[2]))
+import sys
+    if len(sys.argv) == 1:
+        setServoAngle(pan, 90)
+        setServoAngle(tilt, 90)
+    else:
+        setServoAngle(pan, int(sys.argv[1])) # 30 ==> 90 (middle point) ==> 150
+        setServoAngle(tilt, int(sys.argv[2])) # 30 ==> 90 (middle point) ==> 150
     GPIO.cleanup()
