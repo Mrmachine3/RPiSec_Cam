452808111c63e20d901b5bc32d752db668f8edec
mrmachine3 <atone36@gmail.com>
2018-12-30 20:28:53 -0600

    updated autopthat control script

---
+++ auto_pthatctrl.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+###########################################################################
+# Script Name: pthat_cntrl.py
+# Create Date: 12/29/2018
+# Description: The purpose of this script is to remotely control the pan-tilt
+# servo angle of PiCamera. 
+# Author: Mr. Machine
+# Tags: hardware, IoT, servos,  
+########################################################################### 
+
+# Library imports
+import pantilthat as pt
+from time import sleep, time 
+from datetime import datetime
+
+'''
+# Enable code for end user control pattern 
+pan_in = abs(int(input("Enter desired angle between -90 and 90 on the horizontal axis: ")))
+#pan_in = abs(int(45))
+
+# Move servo to both viewpoints of input parameters
+pt.pan(pan_in * -1)
+sleep(ls)
+pt.pan(pan_in)
+sleep(ls)
+'''
+
+#Define sleep time
+s =.5     #short sleep interval
+ls =1.5   #long sleep interval
+
+# Define script start time
+start_time = datetime.utcnow()
+now = start_time.strftime("%H:%M:%S")
+
+# Neutral horizontal and vertical servo postions
+pt.pan(0)
+pt.tilt(90)
+
+# This list defines the servo angle value increments
+pan_presets = [0, -15, -30, -45, -60, -75, -90, -75, -60, -45, -30, -15, 0, 15, 30, 45, 60, 75, 90, 75, 60, 45, 30, 15]
+
+# Begin at farthest viewpoint position on right side and loop through servo angle parameters 
+for x in pan_presets:
+    pt.pan(x)
+    sleep(s)
+for x in pan_presets[::-1]:
+    pt.pan(x)
+    sleep(s)
+    
+# Neutral horizontal and vertical servo postions
+pt.pan(0)
+pt.tilt(90)
+
+# Define script end time
+end_time = datetime.utcnow()
+duration = (end_time-start_time).total_seconds()
+#time = duration.strftime("%H:%M:%S")
+print("Script ran in " + str(round(duration,2)) + " seconds")
